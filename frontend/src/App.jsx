dd26af21ba5faec8b479eb3050288e8f267101ac
trong <cutocm989@gmail.com>
2025-10-27 19:33:19 +0700

    3 commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Login from './page/LoginPage'
 import Home from './page/Home'
 import Info from './page/Info'
