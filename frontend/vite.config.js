6e32c6f45a7c96fff68afc167fb9fd3cfe0cc310
trong <cutocm989@gmail.com>
2025-10-29 08:08:46 +0700

    7commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
-    'process.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(process.env.VITE_GOOGLE_CLIENT_ID),
-    'process.env.VITE_GOOGLE_REDIRECT_URI': JSON.stringify(process.env.VITE_GOOGLE_REDIRECT_URI),
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://blog-test-trong.duckdns.org/api'),
+    'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify('305033698203-9o0n2e2m5flqiks1crns04vm7ce2uvnb.apps.googleusercontent.com'),
+    'import.meta.env.VITE_GOOGLE_REDIRECT_URI': JSON.stringify('https://blog-test-trong.duckdns.org/authenticate'),
   },
 })
